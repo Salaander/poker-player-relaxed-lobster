8c97cfbbb0eaa3eb46a325538f27f6d9a815a3c4
Sándor Nagy <privsala@gmail.com>
2018-10-06 10:35:59 +0200

    test

--- player.py
+++ player.py
@@ -1,9 +1,23 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+    VERSION = "First try"
 
-    def betRequest(self, game_state):
+    def betRequest(self, g):
+
+        #
+        # current_buy_in - players[in_action][bet]
+
+        # current_buy_in - players[in_action][bet] + minimum_raise
+
+        return g["current_buy_in"] - g["palyers"][g["in_action"]][bet] + g["raise"] + 300
+
+    """
+    def _raise(g):
+        return g["current_buy_in"] - g["palyers"][g["in_action"]][bet] + g["raise"]
+
+    def check(self):
         return 0
+    """
 
     def showdown(self, game_state):
         pass
