a973e0e443838516a66907d27602c6ffb3c8b603
Dani Borbas <dani.borbas@gmail.com>
2018-10-06 12:56:48 +0200

    card strength

--- player.py
+++ player.py
@@ -11,14 +11,19 @@ class Player:
         return g["current_buy_in"] - g["players"][g["in_action"]]["bet"] + 500
         #return 0
 
-    """
-    def _raise(g):
-        return g["current_buy_in"] - g["palyers"][g["in_action"]][bet] + g["raise"]
 
-    def check(self):
-        return 0
-    """
+    def strength(cards):
+        return ((value_cards(cards[0]["rank"]) + value_cards(cards[1]["rank"]))/ (2 * 13)) * 10
+
+    def value_cards(rank):
+        switcher = {
+                "J": 11,
+                "Q": 12,
+                "K": 13,
+                "A": 14,
+        }
+        return switcher.get(rank, rank)
+
 
     def showdown(self, game_state):
         pass
-
