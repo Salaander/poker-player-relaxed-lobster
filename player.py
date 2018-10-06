b12d5f68436d722dfe2580d03b96e1fdd03a8192
Sándor Nagy <privsala@gmail.com>
2018-10-06 11:32:35 +0200

    yolo

--- player.py
+++ player.py
@@ -8,14 +8,21 @@ class Player:
         # current_buy_in - players[in_action][bet]
 
         # current_buy_in - players[in_action][bet] + minimum_raise
-        return g["current_buy_in"] - g["players"][g["in_action"]]["bet"] + 500
+        in_action = g["players"][g["in_action"]]
+        raise_amount = 500
+        strength = self.strength(in_action["hole_cards"])
+        if strength > 17:
+            raise_amount += 100
+        if strength > 21:
+            raise_amount += 100
+        return g["current_buy_in"] - g["players"][g["in_action"]]["bet"] + raise_amount
         #return 0
 
 
-    def strength(cards):
-        return ((value_cards(cards[0]["rank"]) + value_cards(cards[1]["rank"]))/ (2 * 13)) * 10
+    def strength(self, cards):
+        return (self.value_cards(cards[0]["rank"]) + self.value_cards(cards[1]["rank"])) #/ (2 * 14)) * 10
 
-    def value_cards(rank):
+    def value_cards(self, rank):
         switcher = {
                 "J": 11,
                 "Q": 12,
